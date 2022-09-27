2fe1301e30180bb444cb282eb2f9a5cf63126352
yuta0709 <yuta0709dev@gmail.com>
2022-09-27 15:35:12 +0900

    fix docker

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-./wait-for-it.sh mysql:3306
+./wait-for-it.sh mysql:3306 --timeout=30 --strict -- mysqladmin ping -h $DB_HOST -u $DB_USER -p${DB_PASSWORD}
 
 java -jar ./api.jar -Duser.timezone=Asia/Tokyo
