6175e8bb07239ce0b67bda48d5855f7f2ef4d4bf
yuta0709 <yuta0709dev@gmail.com>
2022-09-27 15:49:41 +0900

    remove unused import

--- api/src/main/java/com/example/api/repository/IdeaRepository.java
+++ api/src/main/java/com/example/api/repository/IdeaRepository.java
@@ -1,11 +1,8 @@
 package com.example.api.repository;
 
 import com.example.api.model.Idea;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface IdeaRepository extends JpaRepository<Idea, Integer> {
 }
