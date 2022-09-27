e59ec6ca51786b07f2b2bd97abf13de507b195d3
yuta0709 <yuta0709dev@gmail.com>
2022-09-27 15:36:19 +0900

    add example script

---
+++ example/main.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+import re
+import requests
+
+
+API_ORIGIN = "http://localhost:8080"
+
+
+def post_idea(title: str, description: str, deadline: datetime):
+    data = {
+        "title": title,
+        "description": description,
+        "deadline": deadline.isoformat()
+    }
+    res = requests.post(f"{API_ORIGIN}/ideas", json=data)
+    return res.json()
+
+
+def get_ideas():
+    query = {
+        "page": 0,
+        "limit": 20,
+    }
+    res = requests.get(f"{API_ORIGIN}/ideas", json=query)
+    return res.json()
+
+
+def get_idea(id: int):
+    res = requests.get(f"{API_ORIGIN}/ideas/{id}")
+    return res.json()
+
+
+def post_comment(comment: str, idea_id: int):
+    data = {
+        "comment": comment
+    }
+    res = requests.post(f"{API_ORIGIN}/ideas/{idea_id}/comment")
+    return res.json()
+
+
+def main():
+    title = "どこでもドア"
+    description =  "片開き戸を模した道具。目的地を音声や思念などで入力した上で扉を開くと、その先が目的地になる"
+    deadline = datetime(2100, 1, 1)
+    print(post_idea(title, description, deadline))
+
+
+if __name__ == "__main__":
+    main()
