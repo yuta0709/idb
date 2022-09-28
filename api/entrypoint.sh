0f7e65d83afe784ef5b2ed0952804d3c959a86a0
yuta0709 <yuta0709dev@gmail.com>
2022-09-28 14:18:43 +0900

    optimize docker
    イメージの軽量化・mavenのキャッシュ

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 
-./wait-for-it.sh mysql:3306 --timeout=30 --strict -- mysqladmin ping -h $DB_HOST -u $DB_USER -p${DB_PASSWORD}
+# ./wait-for-it.sh mysql:3306 --timeout=30 --strict -- mysqladmin ping -h $DB_HOST -u $DB_USER -p${DB_PASSWORD}
+until mysqladmin ping -h mysql --silent; do
+  echo "waiting for mysql"
+  sleep 2
+done
+
 
 java -jar ./api.jar -Duser.timezone=Asia/Tokyo
